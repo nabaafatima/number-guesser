7e2f718939bb3cf367c78a2b072a6e392fa3a86a
nabaafatima <nabaafatima7097@gmail.com>
2026-05-25 16:42:26 +0500

    made the logic for tries reached 7 and answer is right

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const number = Math.floor(Math.random() * 100) + 1;
+let number = Math.floor(Math.random() * 100) + 1;
 let count = 0;
 function hints() {
     let user = Number(document.getElementById("guess").value);
     const hint = document.getElementById("hint");
-    if (number > user) {
-        hint.textContent = "YOUR GUESS IS LESS THAN THE NUMBER!";
-    } else if (number < user) {
-        hint.textContent = "YOUR GUESS IS HIGHER THAN THE NUMBER!";
-    } else {
-        hint.textContent = "YOU GUESSED IT RIGHT!";
-    }
     count = count + 1;
     const tries = document.getElementById("tries");
     tries.textContent = count;
+    if (number == user) {
+        alert("YOU GUESSED IT RIGHT!");
+        number = Math.floor(Math.random() * 100) + 1;
+        count = 0;
+        tries.textContent = count;
+        hint.textContent = ""; 
+    }
+    else if (count >= 7) {
+        alert("YOU HAVE RUN OUT OF TRIES");
+        number = Math.floor(Math.random() * 100) + 1;
+        count = 0;
+        tries.textContent = count;
+        hint.textContent = "";
+    } 
+    else if (number < user) {
+        hint.textContent = "YOUR GUESS IS HIGHER THAN THE NUMBER!";
+    } else if (number > user) {
+        hint.textContent = "YOUR GUESS IS LESS THAN THE NUMBER!";
+    }
 }
 start.addEventListener('click', hints);
 
